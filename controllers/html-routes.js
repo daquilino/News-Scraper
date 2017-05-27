9b50ee947437a1b250236e26f8a2d5c397849bf9
Douglas Aquilino <douglas_aquilino@yahoo.com>
2017-05-26 22:11:07 -0400

    cleaned up code

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,26 +3,20 @@ const Article = require("../models/Article.js");
 module.exports = function(app){
 
 	app.get('/', function(req, res){
+		
+		Article.find()
+		.populate("comments")
+		.exec(function(err, docs){
 
-		Article.find({}, function(error, doc) {
-	    // Log any errors
-		    if (error) {
-		      console.log(error);
-		    }
-		    // Or send the doc to the browser as a json object
-		    else {
-			    let hbsObject ={
-						article: doc
-				};
+			let hbsObject ={
+				article: docs
+			};
 
-				res.render("index", hbsObject);
-	   		}
-	  });
+			res.render("index", hbsObject);
+		})
+		
 
 	});
 
 }
 
-
-//in here make .getJSON call to api-route route to get articles and comments from mongoose.
-// response in some object sent to index.	
